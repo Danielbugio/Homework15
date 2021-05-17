0d501434e94f1dae33c249641930b9f401e14d80
Daniel Domingues <danielbugio@hotmail.com>
2021-05-17 17:04:00 +0100

    Homework15

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask.templating import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about me.html")
+
+@app.route('/portfolio/')
+def portefolio():
+    return render_template("portfolio.html")
+
+@app.route("/portfolio/Fakebook/")
+def fakebook():
+    return render_template("Fakebook.html")
+
+@app.route("/portfolio/Boogle/")
+def boogle():
+    return render_template("Boogle.html")
+
+
+@app.route("/portfolio/hair salon/")
+def hairsalon():
+    return render_template("hair-salon.html")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
